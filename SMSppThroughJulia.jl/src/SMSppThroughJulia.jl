97f24e84beb098338702fd08cb601dd63a4c7610
U-ATLAS\D34552 <D34552@DSP0961794.atlas.edf.fr>
2025-09-19 11:16:35 +0200

    Improved documentation and added test files

--- SMSppThroughJulia.jl/src/SMSppThroughJulia.jl
+++ SMSppThroughJulia.jl/src/SMSppThroughJulia.jl
@@ -1,31 +1,44 @@
 module SMSppThroughJulia
 
-using Libdl # paquet utilisé pour la manipulation des librairies dynamiques
+using Libdl # This packet is used for handling dynamic libraries
 
-function test()
+"""
+    This function assumes as input a block structured netcdf file in proper SMS++ format having notably the proper description of some Thermal unit
+        See documentation :
+            https://gitlab.com/smspp/smspp-project/-/tree/develop/doc/SMS++%20File%20Format%20Manual?ref_type=heads
+    
+        the string fname is the name of this file
+    
+    The second input is a (the negative of) price vector of appropriate dimension (the number of time steps)
+
+    as output the user recovers the production output of the unit
+"""
+function value_nuclear_on_price( a::Vector{Float64}, fname::String)
     libpath = joinpath(pwd(), "cpp", "emx_smspp_lib")
 
-    # Ouvrir la bibliothèque partagée
+    # Open Shared Library
     libc = Libdl.dlopen(libpath) # ; throw_error=false)
     if libc == C_NULL
-        error("Impossible de charger la bibliothèque : $libpath")
+        error("Impossible to load the library : $libpath")
     end
     
     try
-        # Récupérer le pointeur de fonction
+        # Recover the pointer to the function
         emxThf = dlsym(libc, :emx_calling_thf_C)
         
-        a = [10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0]
+        # vector a = - Market Price 
+        #a = [-10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0]
         n = length(a)
         result_size = Ref{Int32}(0)
         
-        t_file = joinpath(pwd(), "NBlock_ramp.nc4")
+        # The file with the data
+        t_file = joinpath(pwd(), fname)
 
-        # Appel à la fonction C
+        # Calling the underlying C function
         values = ccall(emxThf, Ptr{Cdouble}, (Cstring, Ptr{Cdouble}, Csize_t, Ref{Cint}),
                        t_file, a, n, result_size)
         
-        result_size[] <= 0 && error("La taille du résultat est invalide : $(result_size[])")
+        result_size[] <= 0 && error("Invalid size of result : $(result_size[])")
         
         # Création du tableau Julia à partir des données C (own=true means that Julia own the C ptr)
         result = unsafe_wrap(Array{Float64,1}, values, (result_size[],); own=true)
@@ -33,10 +46,52 @@ function test()
         return result
         
     catch e
-        println("Erreur lors de l'appel à la bibliothèque C : $(e)")
+        println("Some error occured when calling the C library : $(e)")
         throw(e)  # Répendre l'exception pour gérer l'erreur plus haut si nécessaire
     finally
-        # Fermeture de la bibliothèque
+        # close the lib
+        Libdl.dlclose(libc)
+    end
+end
+
+
+function test()
+    libpath = joinpath(pwd(), "cpp", "emx_smspp_lib")
+
+    # Open Shared Library
+    libc = Libdl.dlopen(libpath) # ; throw_error=false)
+    if libc == C_NULL
+        error("Impossible to load the library : $libpath")
+    end
+    
+    try
+        # Recover the pointer to the function
+        emxThf = dlsym(libc, :emx_calling_thf_C)
+        
+        # vector a = - Market Price 
+        a = [-10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0]
+        n = length(a)
+        result_size = Ref{Int32}(0)
+        
+        # The file with the data
+        t_file = joinpath(pwd(), "NBlock_mod3.nc4")
+
+        # Calling the underlying C function
+        values = ccall(emxThf, Ptr{Cdouble}, (Cstring, Ptr{Cdouble}, Csize_t, Ref{Cint}),
+                       t_file, a, n, result_size)
+        
+        result_size[] <= 0 && error("Invalid size of result : $(result_size[])")
+        
+        # Création du tableau Julia à partir des données C (own=true means that Julia own the C ptr)
+        result = unsafe_wrap(Array{Float64,1}, values, (result_size[],); own=true)
+        
+        return result
+        
+    catch e
+        println("Some error occured when calling the C library : $(e)")
+        throw(e)  # Répendre l'exception pour gérer l'erreur plus haut si nécessaire
+    finally
+        # close the lib
         Libdl.dlclose(libc)
     end
 end
